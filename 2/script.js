3ccf2431a7fbbd9adb5437c91453037351d8bceb
Raul Galindo Lopez <raulglprogram@gmail.com>
2023-06-12 16:12:38 +0200

    0.2.2
    - JS ready

---
+++ 2/script.js
@@ -0,0 +1,19 @@
+let buttons = document.querySelectorAll('.buttons > button');
+let counter = document.querySelector('.counter');
+
+var count = 0;
+
+window.onload = function() {
+    for (var i = 0; i < buttons.length; i++) {
+        let buttonName = buttons[i].classList.value;
+        buttons[i].addEventListener('click', function(e) {
+            if (buttonName == 'increase') {
+                counter.textContent = parseInt(counter.textContent) + 1
+            } else if (buttonName == 'decrease') {
+                counter.textContent = parseInt(counter.textContent) - 1
+            } else {
+                counter.textContent = 0
+            }
+        })
+    };
+}
